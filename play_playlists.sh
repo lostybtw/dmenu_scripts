e2dcffd8f4c4d6469296bfa350364c15c6f1327f
lostybtw <58177990+lostybtw@users.noreply.github.com>
2022-03-18 06:55:10 +0000

    added playlist script

---
+++ play_playlists.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+declare -a playlists=(
+"lofi chill ~ https://www.youtube.com/watch?v=d9M-YFxikvI"
+"Advait bedroom pop to calm anxitey ~ https://www.youtube.com/watch?v=K4Icg1K3QE8"
+"sad lofi for lost souls ~ https://www.youtube.com/watch?v=7k9FBNqGhBg"
+"the lonliest feeling in the world ~ https://www.youtube.com/watch?v=7AHGD1IvssY"
+"exit"
+)
+
+choice=$(printf '%s\n' "${playlists[@]}" | dmenu -i -l 20 -p 'play playlists: ')
+
+if [["$choice" == "exit"]];then
+    exit
+else
+    link=$(printf '%s\n' "${choice}" | awk '{print $NF}' )
+    alacritty -e mpv --no-video "$link"
+fi
